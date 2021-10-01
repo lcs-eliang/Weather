bd81fc5bc5697ad7c02c9850df46c6e59581c0a4
Emily Liang <eliang@lcs.on.ca>
2021-10-01 12:11:14 -0400

    added history question next step create history

--- Shared/Views/main.swift
+++ Shared/Views/main.swift
@@ -15,20 +15,38 @@ while true {
     // Show the prediction
     print("Would you like a weather prediction? (Y/N)")
     
-//    let input = readLine()!
+    //    let input = readLine()!
     
     let answer = readLine()!
-
+    
     // Generate a weather prediction
     let prediction = WeatherPredictionGenerator.getPrediction()
     
+    var history: [Prediction] = []
+    
+    
+    
     if answer == "Y" {
         print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
-
+        
         print("That's \(prediction.feel.lowercased())!")
-
+        
+        
         
     }  else if answer == "N" {
-        exit(0)
+        
+        print("Would you like to see the history? (Y/N)")
+        
+        let answer1 = readLine()!
+        
+        if answer1 == "Y" {
+            
+            
+            print("History: \(history)")
+            
+        } else if answer1 == "N" {
+            
         }
+        exit(0)
+    }
 }
